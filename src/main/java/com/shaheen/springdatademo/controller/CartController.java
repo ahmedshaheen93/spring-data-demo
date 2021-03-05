26b0780df083fe75823cdf303b6bb8fc2effde46
ahmed shaheen <ahmedshaheen93@yahoo.com>
2021-03-05 22:39:56 +0200

    create cart controller

---
+++ src/main/java/com/shaheen/springdatademo/controller/CartController.java
@@ -0,0 +1,31 @@
+package com.shaheen.springdatademo.controller;
+
+import com.shaheen.springdatademo.model.Cart;
+import com.shaheen.springdatademo.model.Item;
+import com.shaheen.springdatademo.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    private CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cart> addItems(@RequestBody Set<Item> items) {
+        Cart cart = cartService.addToCart(items);
+        return new ResponseEntity<>(cart, HttpStatus.CREATED);
+    }
+}
